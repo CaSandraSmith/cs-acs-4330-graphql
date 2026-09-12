ead9ca32e7f1939bda93fe2ea8a6622bf7c1f327
Ca'Sandra Smith <casresm3@gmail.com>
2026-09-12 18:47:37 -0400

    Updated server for lesson 4

--- lesson-03/server.js
+++ lesson-03/server.js
@@ -2,6 +2,8 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import 'dotenv/config'
 
+const apikey = process.env.OPENWEATHERMAP_API_KEY
+
 
 const typeDefs = `#graphql
     type Weather {
@@ -25,13 +27,13 @@ const typeDefs = `#graphql
 
     type Query {
         getWeather(zip: Int!, units: Units): Weather!
+        getWeatherByLocation(lat: Float!, lon: Float!, units: Units): Weather!
     }
 `
 
 const resolvers = {
   Query: {
     getWeather: async (_, { zip, units = 'imperial' }) => {
-      const apikey = process.env.OPENWEATHERMAP_API_KEY
       const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
       const res = await fetch(url)
       const json = await res.json()
@@ -51,7 +53,32 @@ const resolvers = {
             pressure: json.main.pressure,
             humidity: json.main.humidity,
             description: json.weather[0].description,
-            name: json.name
+            name: json.name,
+            cod: Number(json?.cod)
+        }
+    },
+    getWeatherByLocation: async (_, { lat, lon, units = 'imperial' }) => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+      const res = await fetch(url)
+      const json = await res.json()
+
+      if (!json.main) {
+        return {
+            message: json?.message,
+            cod: Number(json?.cod)
+        }
+      }
+
+      return { 
+            temperature: json.main.temp,
+            feels_like: json.main.feels_like,
+            temp_min: json.main.temp_min,
+            temp_max: json.main.temp_max,
+            pressure: json.main.pressure,
+            humidity: json.main.humidity,
+            description: json.weather[0].description,
+            name: json.name,
+            cod: Number(json?.cod)
         }
     }
   }
